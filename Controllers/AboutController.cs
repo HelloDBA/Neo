8d9bcb759190f9c2c3e7d705f6f67575297a3b50
HelloDBA <hellodba@live.com>
2016-09-22 17:11:02 +0800

    add AboutControllers

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace neo.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: /<controller>/
+        public string Index(string name, int id=1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name},ID:{id}");
+
+        }
+    }
+}
